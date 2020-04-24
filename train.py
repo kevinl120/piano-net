9616472e85789444b48884c9b612120b079a06df
Kevin Li <kevinli120@yahoo.com>
2020-04-24 01:20:18 -0700

    Add dense layer to each finger

--- train.py
+++ train.py
@@ -14,14 +14,16 @@ def custom_crossentropy(ytrue, ypred):
 
 def create_model():
     inputs = Input(shape=(30, 3))
-    x = Conv1D(16, 3, padding='same')(inputs)
+    x = inputs
+    x = Conv1D(64, 3, padding='same')(x)
     x = ReLU()(x)
     x = Flatten()(x)
     x = Dense(64, activation='relu')(x)
 
     finger_outputs = []
     for i in range(20):
-        finger_out = Dense(10, activation='softmax', name='note{}_finger1_dense'.format(i))(x)
+        finger_out = Dense(64, activation='relu')(x)
+        finger_out = Dense(10, activation='softmax', name='note{}_finger1_dense'.format(i))(finger_out)
         finger_outputs.append(finger_out)
     outputs = concatenate(finger_outputs)
     model = Model(inputs=[inputs], outputs=[outputs])
@@ -30,14 +32,14 @@ def create_model():
 def train():
     model = create_model()
     x_train, y_train = from_fingering_file()
-    # opt = Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, amsgrad=False)
-    # model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
-    model.compile(loss=custom_crossentropy, optimizer=Adam(), metrics=['accuracy'])
+    opt = Adam(learning_rate=0.0001, beta_1=0.9, beta_2=0.999, amsgrad=False)
+    model.compile(loss=custom_crossentropy, optimizer=opt, metrics=['accuracy'])
+    print(model.summary())
 
     # import pdb
     # pdb.set_trace()
     # print(model.predict(x_train[0].reshape((-1, 30, 3))))
 
-    model.fit(x=x_train, y=y_train, batch_size=32, epochs=50)
+    model.fit(x=x_train, y=y_train, batch_size=32, epochs=300)
 
 train()
