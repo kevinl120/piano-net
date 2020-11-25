11ac7bd1b414d0d3fbdeea4dd4f21b7a64646971
Kevin Li <kevinli120@yahoo.com>
2020-11-25 14:35:14 -0800

    Predict hands separately

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,7 @@
 import glob
 import numpy as np
 import os
+import pandas as pd
 
 from keras.utils import to_categorical
 
@@ -23,29 +24,40 @@ def pitch_to_num(note):
     return res
 
 
-def from_fingering_file(start_file=0):
-    files = glob.glob('/kaggle/input/pianofingerings/FingeringFiles/*-1_fingering.txt')
+def get_finger(f, two_hand=False):
+    f = str(f)
+    finger = [int(x) for x in f.split('_')][0]
+    if two_hand:
+        finger = finger + (5 if finger < 0 else 4)
+    else:
+        finger = abs(finger) - 1
+    return finger
+
+
+def from_fingering_file(context=5, two_hand=False):
+    files = glob.glob('data/FingeringFiles/*-1_fingering.txt')
     files.sort()
 
     x_train = []
     y_train = []
-
-    for path in files[start_file:]:
-        with open(path) as f:
-            all_lines = [line for line in f.readlines() if line[0] != '/']
-            x_file = []
-            y_file = []
-            for line in all_lines:
-                l = line.split('\t')
-                x_file.append([float(l[1]), float(l[2]), pitch_to_num(l[3])])
-
-                fingering = [int(x) for x in l[7].split('_')][0]
-                fingering = fingering + (5 if fingering < 0 else 4)
-                y_file.append(fingering)
-            for i in range(len(y_file) - 30):
-                x_train.append(np.array(x_file[i:i+30]))
-                y_train.append(to_categorical(y_file[i+15], num_classes=10))
-    x_train = np.array(x_train)
-    y_train = np.array(y_train)
-
-    return x_train, y_train
+    
+    col_names = ['noteID', 'onsetTime', 'offsetTime', 'pitch', 'onsetVelocity', 'offsetVelocity', 'channel', 'finger']
+    
+    for path in files:
+        
+        df = pd.read_csv(path, sep='\t', header=0, names=col_names)
+        df['pitch'] = df['pitch'].apply(pitch_to_num)
+        df['finger'] = df['finger'].apply(lambda x: get_finger(x, two_hand=two_hand))
+        rh_df = df[df['channel'] == 0]
+        
+        padding = pd.DataFrame([[0]*len(col_names) for i in range(context)], columns=col_names)
+        df = pd.concat([padding, rh_df, padding])
+        df.reset_index(drop=True, inplace=True)
+        x = df[['onsetTime', 'offsetTime', 'pitch']]
+        y = df[['finger']]
+        
+        for i in range(len(df) - (2 * context)):
+            x_train.append(x.iloc[i:i+(2*context)+1].to_numpy())
+            y_train.append(to_categorical(y.iloc[i+context].to_numpy()[0], num_classes=(10 if two_hand else 5)))
+        
+    return np.array(x_train), np.array(y_train)
--- train.py
+++ train.py
@@ -1,37 +1,47 @@
 from preprocess import from_fingering_file
 
-import keras.backend as K
 from keras.layers import Input, Dense, Flatten, Conv1D, ReLU, concatenate
 from keras.models import Model, Sequential
 from keras.optimizers import Adam, SGD
+from sklearn.metrics import accuracy_score
 
+import matplotlib.pyplot as plt
+import numpy as np
 
 def create_model():
-    inputs = Input(shape=(30, 3))
+    inputs = Input(shape=(2*context+1, 3))
     x = inputs
-    x = Conv1D(64, 3, padding='same')(x)
+    x = Conv1D(128, 3, padding='valid')(x)
     x = ReLU()(x)
-    x = Conv1D(64, 3, padding='same')(x)
+    x = Conv1D(256, 3, padding='valid')(x)
+    x = ReLU()(x)
+    x = Conv1D(512, 3, padding='valid')(x)
+    x = ReLU()(x)
+    x = Conv1D(1024, 3, padding='valid')(x)
     x = ReLU()(x)
     x = Flatten()(x)
 
-    x = Dense(64, activation='relu')(x)
-    finger_out = Dense(10, activation='softmax')(x)
+    x = Dense(256, activation='relu')(x)
+    finger_out = Dense((10 if two_hand else 5), activation='softmax')(x)
     model = Model(inputs=[inputs], outputs=finger_out)
     return model
 
 def train():
     model = create_model()
-    x_train, y_train = from_fingering_file(start_file=0)
     opt = Adam(learning_rate=0.001)
-#     opt = Adam(learning_rate=0.0001, beta_1=0.9, beta_2=0.999, amsgrad=False)
     model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
     print(model.summary())
-
-    print(model.predict(x_train[0].reshape((-1, 30, 3))))
-    # import pdb
-    # pdb.set_trace()
-
+    
+    print('accuracy before training:', accuracy_score(np.argmax(y_train, axis=1), np.argmax(model.predict(x_train), axis=1)))
+    
+#     opt = Adam(learning_rate=0.0001, beta_1=0.9, beta_2=0.999, amsgrad=False)
+#     plt.hist(np.argmax(y_train, axis=1), weights=np.ones(len(y_train)) / len(y_train))
+    
     model.fit(x=x_train, y=y_train, batch_size=32, epochs=10, validation_split=0.2)
     
-train()
+    return model
+    
+context = 5
+two_hand = False
+x_train, y_train = from_fingering_file(context=context, two_hand=two_hand)
+m = train()
